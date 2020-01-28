d4ce5d281c453d423f0a9fa1c0528378351cbc54
Christasca <c.tasca4@campus.unimib.it>
2020-01-29 00:15:39 +0100

    Inserito test per verificare il set.

--- src/test/java/com/unimib/smarthome/console/CLITest.java
+++ src/test/java/com/unimib/smarthome/console/CLITest.java
@@ -1,14 +1,53 @@
 package com.unimib.smarthome.console;
 
+import static org.awaitility.Awaitility.await;
+
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import com.unimib.smarthome.SmartHome;
+import com.unimib.smarthome.entity.EntityManager;
+
+
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CLITest {
-	CLIEvaluation eval = new CLIEvaluation();
+	public CLIEvaluation eval = new CLIEvaluation();
+	private static boolean setUpIsDone = false;
+	
+	static EntityManager em = EntityManager.getInstance();
 	
 	
+	@BeforeAll
+	protected static void setUp() {
+		if (setUpIsDone) {
+	        return;
+	    }
+	    setUpIsDone = true;
+		
+		SmartHome.main(null);
+		
+		
+	}
+
+	private Callable<Boolean> entityHasState(int entityID, String state) {
+	      return () -> em.getEntityState(entityID).equals(state);
+	}
+
+	@Test
 	
-	
+	void testSet() {
+		String test = "set 12 1";
+		eval.evaluation(test);
+		await().atMost(2, TimeUnit.SECONDS).until(entityHasState(12, "1")); 
+	}
 
 }
