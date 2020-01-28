7a6ed9e6ca16cf903f8c72e53cd8ed5448bb9f9d
Christasca <c.tasca4@campus.unimib.it>
2020-01-28 22:49:12 +0100

    Risoluzione nome sbagliato nel file Json.

---
+++ src/test/java/com/unimib/smarthome/console/CLITest.java
@@ -0,0 +1,14 @@
+package com.unimib.smarthome.console;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CLITest {
+	CLIEvaluation eval = new CLIEvaluation();
+	
+	
+	
+	
+
+}
